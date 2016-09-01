b630e80b4720326c0afe5df9b2f66292840947f5
payatsu <6873447+payatsu@users.noreply.github.com>
2016-09-01 23:05:28 +0900

    convert_archivesの体裁変更のバグつぶし。

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -479,8 +479,8 @@ set_variables()
 
 convert_archives()
 {
-	find ${prefix}/src -mindepth 2 -maxdepth 2 -name '*.tar.gz'  -exec sh -c '[ -f `basename {} .gz`.xz  ] && exit 0; echo {}:; gzip  -cd {} | xz -cv > `basename {} .gz`.xz'  \; -delete || return 1
-	find ${prefix}/src -mindepth 2 -maxdepth 2 -name '*.tar.bz2' -exec sh -c '[ -f `basename {} .bz2`.xz ] && exit 0; echo {}:; bzip2 -cd {} | xz -cv > `basename {} .bz2`.xz' \; -delete || return 1
+	find ${prefix}/src -mindepth 2 -maxdepth 2 -name '*.tar.gz'  -exec sh -c 'xzfile=`echo {} | sed -e "s/\\.gz\$/.xz/"`;  [ -f ${xzfile} ] && exit 0; echo converting {} into ${xzfile} ...; gzip  -cd {} | xz -cv > ${xzfile}' \; -delete || return 1
+	find ${prefix}/src -mindepth 2 -maxdepth 2 -name '*.tar.bz2' -exec sh -c 'xzfile=`echo {} | sed -e "s/\\.bz2\$/.xz/"`; [ -f ${xzfile} ] && exit 0; echo converting {} into ${xzfile} ...; bzip2 -cd {} | xz -cv > ${xzfile}' \; -delete || return 1
 	find ${prefix}/src -mindepth 2 -maxdepth 2 -name '*.zip'  -execdir sh -c '[ -f `basename {} .zip`.tar.xz ] && exit 0; unzip {} && tar cJvf `basename {} .zip`.tar.xz `basename {} .zip`' \; -delete || return 1
 }
 
