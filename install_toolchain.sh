1cc7f246096cb1392201445db30b642553f26d3d
payatsu <6873447+payatsu@users.noreply.github.com>
2017-08-11 08:14:20 +0900

    install_toolchain.shのcleanを微修正。

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -937,7 +937,7 @@ clean()
 	find ${prefix}/src -mindepth 2 -maxdepth 2 \
 		! -name '*.tar.gz' ! -name '*.tar.bz2' ! -name '*.tar.xz' ! -name '*.zip' ! -name '*-git' -exec rm -fvr {} +
 	find ${prefix}/src -mindepth 2 -maxdepth 2 \
-		-name '*-git' -exec sh -c "cd {}; make -j ${jobs} clean" \;
+		-name '*-git' -exec sh -c "make -C {} -j ${jobs} clean" \;
 }
 
 strip()
