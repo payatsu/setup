2cad47a0a00ce016a6a61a930331687b5f3db994
payatsu <6873447+payatsu@users.noreply.github.com>
2017-12-27 01:36:00 +0900

    不要なmkdirを削除した。

--- generate_autotools_package.sh
+++ generate_autotools_package.sh
@@ -8,26 +8,26 @@ mkdir -p${verbose:+v} include src || return
 
 int main(void)
 {
-    return 0;
+	return 0;
 }
 EOF
 
 autoscan ${verbose:+-v} || return
 sed -e '
-    /^AC_INIT/{
-        s/FULL-PACKAGE-NAME/'${package_name}'/
-        s/VERSION/0.0.1/
-        aAC_CONFIG_AUX_DIR([config])
-        aAM_INIT_AUTOMAKE
-    }
-    /^# Checks for programs\.$/{
-        aAC_PROG_INSTALL
-        aAC_PROG_LIBTOOL
-    }
-    ${
-        iAC_CONFIG_MACRO_DIR([m4])
-        iAC_CONFIG_FILES([Makefile include/Makefile src/Makefile])
-    }
+	/^AC_INIT/{
+		s/FULL-PACKAGE-NAME/'${package_name}'/
+		s/VERSION/0.0.1/
+		aAC_CONFIG_AUX_DIR([config])
+		aAM_INIT_AUTOMAKE
+	}
+	/^# Checks for programs\.$/{
+		aAC_PROG_INSTALL
+		aAC_PROG_LIBTOOL
+	}
+	${
+		iAC_CONFIG_MACRO_DIR([m4])
+		iAC_CONFIG_FILES([Makefile include/Makefile src/Makefile])
+	}
 ' configure.scan > configure.ac || return
 rm ${verbose:+-v} configure.scan || return
 
@@ -45,37 +45,36 @@ libtoolize -c || return
 aclocal ${verbose:+--verbose} || return
 autoheader ${verbose:+-v} || return
 
-mkdir -p${verbose:+v} config || return
 touch NEWS README AUTHORS ChangeLog COPYING || return
 automake -ac${verbose:+v} || return
 
 autoconf ${verbose:+-v} || return
 
 which git > /dev/null 2>&1 && {
-    cat << EOF > .gitignore || return
-*.o
-*.swp
-*~
-.deps
-.libs
-Makefile
-Makefile.in
-aclocal.m4
-autom4te.cache
-autoscan.log
-config.h
-config.h.in
-config.log
-config.status
-configure
-libtool
-stamp-h1
-EOF
-    git rev-parse > /dev/null 2>&1 || git init . || return
-    git add .gitignore || return
-    git add config m4 || return
-    git add AUTHORS COPYING ChangeLog INSTALL NEWS README || return
-    git add configure.ac Makefile.am include/Makefile.am src/Makefile.am src/main.cpp || return
+	cat <<- EOF > .gitignore || return
+	*.la
+	*.lo
+	*.o
+	*.swp
+	*~
+	.deps
+	.libs
+	Makefile
+	Makefile.in
+	aclocal.m4
+	autom4te.cache
+	autoscan.log
+	config.h
+	config.h.in
+	config.log
+	config.status
+	configure
+	libtool
+	stamp-h1
+	EOF
+	git rev-parse > /dev/null 2>&1 || git init . || return
+	git add .gitignore || return
+	git add config m4 || return
+	git add AUTHORS COPYING ChangeLog INSTALL NEWS README || return
+	git add configure.ac Makefile.am include/Makefile.am src/Makefile.am src/main.cpp || return
 }
-
-# vim: set expandtab shiftwidth=0 tabstop=4 :
