9222c234b87b0690f9b7e280a80fad02377f7a54
payatsu <6873447+payatsu@users.noreply.github.com>
2018-01-19 02:09:46 +0900

    単体テストフレームワークを組み込む仕組みをgenerate_autotools_package に入れた。

--- generate_autotools_package.sh
+++ generate_autotools_package.sh
@@ -2,13 +2,22 @@
 
 : ${package_name:=sample}
 
-mkdir -p${verbose:+v} include src || return
+mkdir -p${verbose:+v} include src test || return
 cat << EOF > src/main.cpp || return
 #include "config.h"
 
 int main(void)
 {
 	return 0;
+}
+EOF
+cat << EOF > test/test.cpp || return
+#include "config.h"
+#include "gtest/gtest.h"
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 EOF
 
@@ -26,19 +35,45 @@ sed -e '
 	}
 	${
 		iAC_CONFIG_MACRO_DIR([m4])
-		iAC_CONFIG_FILES([Makefile include/Makefile src/Makefile])
+		iAC_CONFIG_FILES([Makefile include/Makefile src/Makefile test/Makefile])
 	}
 ' configure.scan > configure.ac || return
 rm ${verbose:+-v} configure.scan || return
 
 cat << EOF > Makefile.am || return
-SUBDIRS = include src
+SUBDIRS = include src test
 ACLOCAL_AMFLAGS = -I m4
 EOF
 touch include/Makefile.am || return
 cat << EOF > src/Makefile.am || return
 bin_PROGRAMS = ${package_name}
 ${package_name}_SOURCES = main.cpp
+EOF
+cat << EOF > test/Makefile.am || return
+noinst_PROGRAMS = testsuite
+testsuite_SOURCES = test.cpp
+nodist_testsuite_SOURCES  = gtest/gtest.h gtest/gtest-all.cc
+testsuite_CPPFLAGS = -I../src
+testsuite_CXXFLAGS = -std=c++11 -Wall -Wextra --coverage
+testsuite_LDFLAGS = -lpthread
+
+gtest_ver = release-1.8.0
+
+gtest/gtest.h gtest/gtest-all.cc: googletest-\$(gtest_ver)
+	\$^/googletest/scripts/fuse_gtest_files.py \$^/googletest .
+
+googletest-\$(gtest_ver):
+	wget --no-check-certificate -O - https://github.com/google/googletest/archive/\$(gtest_ver).tar.gz | tar xzvf -
+
+check:
+	./testsuite
+	lcov -c -d . -o tracefile.info
+	genhtml -p \$(dir \$(CURDIR)) -o html -s tracefile.info
+	\$(RM) tracefile.info *.gcda
+
+.PHONY: myclean
+clean: myclean
+	\$(RM) -r html *.gcda *.gcno
 EOF
 
 libtoolize -c || return
@@ -53,6 +88,8 @@ autoconf ${verbose:+-v} || return
 ! which git > /dev/null 2>&1 && return
 
 cat << EOF > .gitignore || return
+*.gcda
+*.gcno
 *.la
 *.lo
 *.o
@@ -81,4 +118,4 @@ git rev-parse > /dev/null 2>&1 || git init . || return
 git add .gitignore || return
 git add config m4 || return
 git add AUTHORS COPYING ChangeLog INSTALL NEWS README || return
-git add configure.ac Makefile.am include/Makefile.am src/Makefile.am src/main.cpp || return
+git add configure.ac Makefile.am include/Makefile.am src/Makefile.am src/main.cpp test/Makefile.am test/test.cpp || return
