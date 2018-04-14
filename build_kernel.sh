1521ce0ede90bd4e72756ab7539ea815246d3d3e
payatsu <6873447+payatsu@users.noreply.github.com>
2018-04-14 18:29:15 +0900

    perfをインストールできるようにした。

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
-# TODO: perf
 # TODO: pdfdocs htmldocs
+
+help()
+{
+	cat <<EOF
+[NAME]
+	`basename ${0} - build kernel`
+[SYNOPSIS]
+	`basename ${0}` [-c] [-g] [-h] [-k] [-m] [-p] [-s] [-t]
+[OPTIONS]
+	-c
+		install cgroup.
+	-g
+		install gpio.
+	-h
+		install headers.
+	-k
+		build kernel.
+	-m
+		install modules.
+	-p
+		install perf.
+	-s
+		install spi.
+	-t
+		create tags.
+EOF
+}
 
 init()
 {
@@ -11,6 +37,7 @@ init()
 	: ${CROSS_COMPILE:=arm-none-linux-gnueabi-}
 	: ${INSTALL_HDR_PATH:=./usr}
 	: ${INSTALL_MOD_PATH:=/}
+	: ${prefix:=`pwd`}
 	make_opts="-j ${jobs} V=1 W=1 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_HDR_PATH=${INSTALL_HDR_PATH} INSTALL_MOD_PATH=${INSTALL_MOD_PATH}"
 }
 
@@ -31,22 +58,57 @@ fetch()
 
 build()
 {
-	[ -f .config ] || make ${make_opts} olddefconfig || return
-	make ${make_opts} || return
-	make ${make_opts} modules_install || return
-	make ${make_opts} headers_install || return
-	make ${make_opts} tags gtags || return
-	make ${make_opts} -C tools cgroup || return
-	make ${make_opts} -C tools gpio || return
-	make ${make_opts} -C tools perf || return
-	make ${make_opts} -C tools spi || return
+	[ -z "${kernel_build}" ] || {
+		[ -f .config ] || make ${make_opts} olddefconfig || return
+		make ${make_opts} || return
+	}
+	[ -z "${modules_install}" ] || make ${make_opts} modules_install || return
+	[ -z "${headers_install}" ] || make ${make_opts} headers_install || return
+	[ -z "${tags_create}" ] || make ${make_opts} tags gtags || return
+	[ -z "${cgroup_install}" ] || {
+		make ${make_opts} -C tools cgroup || return
+	}
+	[ -z "${gpio_install}" ] || {
+		make ${make_opts} -C tools gpio || return
+	}
+	[ -z "${perf_install}" ] || {
+		make ${make_opts} -C tools/perf all || return
+		make ${make_opts} -C tools/perf doc || return
+		make ${make_opts} -C tools/perf DESTDIR=${prefix} install || return
+	}
+	[ -z "${spi_install}" ] || {
+		make ${make_opts} -C tools spi || return
+	}
 }
 
 main()
 {
+	cgroup_install=
+	gpio_install=
+	headers_install=
+	kernel_build=
+	modules_install=
+	perf_install=
+	spi_install=
+	tags_create=
+	while getopts cghkmpst arg; do
+		case ${arg} in
+		c) cgroup_install=yes;;
+		g) gpio_install=yes;;
+		h) headers_install=yes;;
+		k) kernel_build=yes;;
+		m) modules_install=yes;;
+		p) perf_install=yes;;
+		s) spi_install=yes;;
+		t) tags_create=yes;;
+		\?) help 2>&1; exit 1;;
+		esac
+	done
+	shift `expr ${OPTIND} - 1`
+
 	init || return
 	fetch || return
 	build || return
 }
 
-main
+main "$@"
