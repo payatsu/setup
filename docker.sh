7b82d20ef0a86e40e7dd95bc8a8f742332b0861d
payatsu <6873447+payatsu@users.noreply.github.com>
2018-07-25 00:22:49 +0900

    docker.shをDebianとUbuntuで共通化した。

--- docker.sh
+++ docker.sh
@@ -3,10 +3,10 @@
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z)/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z) $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce
 
