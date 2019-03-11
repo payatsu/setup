144eada7affd1d59c6eee8fd5d90b6d08a4577ae
payatsu <6873447+payatsu@users.noreply.github.com>
2019-03-11 21:30:36 +0900

    modify docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sudo apt update || exit
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common || exit
+apt update || exit
+apt install -y apt-transport-https ca-certificates curl software-properties-common || exit
 
-curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z)/gpg | sudo apt-key add - || exit
-sudo apt-key fingerprint 0EBFCD88 || exit
+curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z)/gpg | apt-key add - || exit
+apt-key fingerprint 0EBFCD88 || exit
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z) $(lsb_release -cs) stable" || exit
-sudo apt update || exit
-sudo apt install -y docker-ce || exit
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z) $(lsb_release -cs) stable" || exit
+apt update || exit
+apt install -y docker-ce || exit
 
-sudo usermod -aG docker ${SUDO_USER:-${USER}} || exit
+usermod -aG docker ${SUDO_USER:-${USER:-`whoami`}} || exit
