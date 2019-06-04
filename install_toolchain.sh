2c093317ff694948392d6b23a29f66d38d17148f
payatsu <6873447+payatsu@users.noreply.github.com>
2019-06-05 00:02:46 +0900

    vim version up.

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -75,7 +75,7 @@
 : ${libffi_ver:=3.2.1}
 : ${emacs_ver:=26.2}
 : ${libiconv_ver:=1.15}
-: ${vim_ver:=8.1.1027}
+: ${vim_ver:=8.1.1462}
 : ${vimdoc_ja_ver:=dummy}
 : ${ctags_ver:=git}
 : ${grep_ver:=3.3}
