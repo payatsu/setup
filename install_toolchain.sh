affab99cb5c1cadfc60daf5ebfc701901fe78026
payatsu <6873447+payatsu@users.noreply.github.com>
2019-09-20 22:05:28 +0900

    llvm version up.

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -129,7 +129,7 @@
 : ${ccache_ver:=3.7.2}
 : ${libedit_ver:=20181209-3.1}
 : ${swig_ver:=3.0.12}
-: ${llvm_ver:=8.0.0}
+: ${llvm_ver:=9.0.0}
 : ${compiler_rt_ver:=${llvm_ver}}
 : ${libunwind_ver:=${llvm_ver}}
 : ${libcxxabi_ver:=${llvm_ver}}
