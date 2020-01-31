54b212a469366b427dbbf69471a1db14f649edf0
payatsu <6873447+payatsu@users.noreply.github.com>
2020-01-31 21:07:38 +0900

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-apt update || exit
-apt install -y apt-transport-https ca-certificates curl software-properties-common || exit
+apt-get update || exit
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common || exit
 
 curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z)/gpg | apt-key add - || exit
 apt-key fingerprint 0EBFCD88 || exit
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr A-Z a-z) $(lsb_release -cs) stable" || exit
-apt update || exit
-apt install -y docker-ce || exit
+apt-get update || exit
+apt-get install -y docker-ce || exit
 
 usermod -aG docker ${SUDO_USER:-${USER:-`whoami`}} || exit
