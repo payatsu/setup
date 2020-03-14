ee0e55643a05bcfde577929e1b6d33606559efae
payatsu <6873447+payatsu@users.noreply.github.com>
2020-03-14 15:36:41 +0900

    rustc version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -157,7 +157,7 @@
 : ${boost_ver:=1_72_0}
 : ${Python_ver:=3.8.2}
 : ${Python2_ver:=2.7.17}
-: ${rustc_ver:=1.41.0}
+: ${rustc_ver:=1.42.0}
 : ${rustup_ver:=1.21.1}
 : ${ruby_ver:=2.7.0}
 : ${go_ver:=1.14}
