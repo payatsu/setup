b62fb68fb0beb63762465373af15b0dc9255e2cf
payatsu <6873447+payatsu@users.noreply.github.com>
2020-07-07 23:26:19 +0900

    swig version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -146,7 +146,7 @@
 : ${Bear_ver:=2.4.3}
 : ${ccache_ver:=3.7.9}
 : ${libedit_ver:=20181209-3.1}
-: ${swig_ver:=4.0.1}
+: ${swig_ver:=4.0.2}
 : ${llvm_ver:=10.0.0}
 : ${compiler_rt_ver:=${llvm_ver}}
 : ${libunwind_ver:=${llvm_ver}}
