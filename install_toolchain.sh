c357965d168dfb3b9aa62d17702a78cda0f0ad1e
payatsu <6873447+payatsu@users.noreply.github.com>
2020-08-05 18:33:16 +0900

    python version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -159,7 +159,7 @@
 : ${cling_ver:=git}
 : ${ccls_ver:=git}
 : ${boost_ver:=1_73_0}
-: ${Python_ver:=3.8.3}
+: ${Python_ver:=3.8.5}
 : ${Python2_ver:=2.7.18}
 : ${rustc_ver:=1.45.2}
 : ${rustup_ver:=1.22.1}
