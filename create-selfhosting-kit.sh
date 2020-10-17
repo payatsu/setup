e97b0e932eae6b008be0c2592c0640f0df393c9e
payatsu <6873447+payatsu@users.noreply.github.com>
2020-10-17 22:12:57 +0900

    update pathconfig generation in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -2215,7 +2215,7 @@ generate_pathconfig()
 	mkdir -pv `dirname ${1}` || return
 	func_name=`basename ${1} | tr . _ | tr -cd '[:alpha:]_'`
 	cat << 'EOF' | sed -e '1,/^{$/{s%prefix_place_holder%'${prefix}'%;s%host_place_holder%'${host}'%;s%func_place_holder%'${func_name}'%};$s%func_place_holder%'${func_name}'%' > ${1} || return
-prefix=prefix_place_holder
+prefix=$({ cd $(dirname ${BASH_SOURCE:-${(%):-%N}}) && pwd || echo prefix_place_holder;} | sed -e "s!^${DESTDIR}!!")
 host=host_place_holder
 func_place_holder()
 {
