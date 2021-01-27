91522245a7843ed4fb9b14316a140f42468fc407
payatsu <6873447+payatsu@users.noreply.github.com>
2021-01-27 20:46:51 +0900

    python version up in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -119,7 +119,7 @@ EOF
 : ${expat_ver:=2.2.9}
 : ${libffi_ver:=3.3}
 : ${openssl_ver:=1.1.1i}
-: ${Python_ver:=3.9.0}
+: ${Python_ver:=3.9.1}
 : ${boost_ver:=1_75_0}
 : ${source_highlight_ver:=3.1.9}
 : ${bzip2_ver:=1.0.8}
