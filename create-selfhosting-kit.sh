a9807237abf0ffa5d5b274f0a314c44076b20f30
payatsu <6873447+payatsu@users.noreply.github.com>
2021-02-26 21:54:19 +0900

    nano version up

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -183,7 +183,7 @@ EOF
 : ${vim_ver:=8.2.2425}
 : ${vimdoc_ja_ver:=master}
 : ${emacs_ver:=27.1}
-: ${nano_ver:=5.5}
+: ${nano_ver:=5.6}
 : ${ctags_ver:=git}
 : ${grep_ver:=3.6}
 : ${diffutils_ver:=3.7}
