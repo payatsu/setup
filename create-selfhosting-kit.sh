03bb18bef62b61a2636211f806c4f60986e9e611
payatsu <6873447+payatsu@users.noreply.github.com>
2021-04-14 23:32:47 +0900

    openssl version up

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -108,7 +108,7 @@ EOF
 : ${bzip2_ver:=1.0.8}
 : ${xz_ver:=5.2.5}
 : ${zstd_ver:=1.4.9}
-: ${openssl_ver:=1.1.1j}
+: ${openssl_ver:=1.1.1k}
 : ${curl_ver:=7.75.0}
 : ${elfutils_ver:=0.183}
 : ${binutils_ver:=2.35.2}
