b35db42b0d8fcf0ad610719e9588d909116dc0b5
payatsu <6873447+payatsu@users.noreply.github.com>
2021-08-01 15:37:06 +0900

    update generate_autotools_package.sh

--- generate_autotools_package.sh
+++ generate_autotools_package.sh
@@ -33,6 +33,8 @@ sed -e '
 	}
 	/^# Checks for programs\.$/{
 		aAC_PROG_INSTALL
+		aAC_PROG_GREP
+		aAC_PROG_SED
 		# aLT_INIT
 	}
 	/^# Checks for libraries\.$/{
@@ -43,11 +45,18 @@ sed -e '
 		iAC_CONFIG_FILES([Makefile src/Makefile tests/Makefile])
 		i[warning_options="-Wextra -Wcast-align -Wstrict-aliasing -Wshadow "\\
 		i`LANG=C ${CXX} -fsyntax-only -Q --help=warnings,^joined,^separate,common --help=warnings,^joined,^separate,c++ |
-		igrep -v '\''\\@<:@enabled\\@:>@\\|-Wabi\\|-Waggregate-return\\|-Wchkp\\|-Wc90-c99-compat\\|-Wredundant-tags\\|-Wsuggest-attribute=const\\|-Wsystem-headers\\|-Wtemplates\\|-Wtraditional@<:@^-@:>@'\'' | grep -oe '\''-W@<:@@<:@:graph:@:>@@:>@\\+'\'' | sed -e '\''s/<@<:@0-9,@:>@\\+>//'\'' | sed -e '\''$!s/$/ \\\\\\\\/'\''`]
+		i${GREP} -v '\''\\@<:@enabled\\@:>@\\|-Wabi\\|-Waggregate-return\\|-Wchkp\\|-Wc90-c99-compat\\|-Wnamespaces\\|-Wredundant-tags\\|-Wsuggest-attribute=const\\|-Wsystem-headers\\|-Wtemplates\\|-Wtraditional@<:@^-@:>@'\'' |
+		i${GREP} -oe '\''-W@<:@@<:@:graph:@:>@@:>@\\+'\'' |
+		i${SED} -e '\''s/<@<:@0-9,@:>@\\+>//'\'' |
+		i${SED} -e '\''$!s/$/ \\\\\\\\/'\''`]
 		iAC_SUBST([warning_options])
-		i[system_include_dirs=`LANG=C ${CPP} ${CPPFLAGS} -v -x c /dev/null -o /dev/null 2>&1 | sed -e '\''/^#include "/,/^End of search list\\.$/p;d'\'' | sed -e '\''/^ /{s///;s/$/\\/\\\\\\\\*/;p};d'\'' | sed -e '\''$!s/$/ \\\\\\\\/'\''`]
+		i[system_include_dirs=`LANG=C ${CPP} ${CPPFLAGS} -v -x c /dev/null -o /dev/null 2>&1 |
+		i${SED} -e '\''/^#include "/,/^End of search list\\.$/p;d'\'' |
+		i${SED} -e '\''/^ /{s///;s/$/\\/\\\\\\\\*/;p};d'\'' |
+		i${SED} -e '\''$!s/$/ \\\\\\\\/'\''`]
 		iAC_SUBST([system_include_dirs])
-		iAC_ARG_ENABLE([sanitizer], [AS_HELP_STRING([--enable-sanitizer], [enable sanitizer])], [AS_VAR_IF([enable_sanitizer], [yes], [sanitizer_flags='\''-static-libasan -static-liblsan -static-libubsan'\'']) AC_SUBST([sanitizer_flags])])
+		iAC_ARG_ENABLE([sanitizer],
+		i\	[AS_HELP_STRING([--enable-sanitizer], [enable sanitizer])], [AS_VAR_IF([enable_sanitizer], [yes], [sanitizer_flags='\''-static-libasan -static-liblsan -static-libubsan'\'']) AC_SUBST([sanitizer_flags])])
 	}
 ' configure.scan > configure.ac || return
 rm ${verbose:+-v} configure.scan || return
