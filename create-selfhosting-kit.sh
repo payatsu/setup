e968024ae344ff9877323043419832cef104bac7
payatsu <6873447+payatsu@users.noreply.github.com>
2021-08-16 17:35:31 +0900

    fix typo in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -1902,7 +1902,7 @@ EOF
 		;;
 	autoconf)
 		[ -x ${DESTDIR}${prefix}/bin/autoconf -a "${force_install}" != yes ] && return
-		print_binary_path autoconf > /dev/null || ${0} ${cmdopt} m4 || return
+		print_binary_path m4 > /dev/null || ${0} ${cmdopt} m4 || return
 		fetch ${1} || return
 		unpack ${1} || return
 		[ -f ${autoconf_bld_dir}/Makefile ] ||
