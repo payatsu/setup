7ccc033299e431a074965609e72c498eaf1d4839
payatsu <6873447+payatsu@users.noreply.github.com>
2021-12-26 01:01:17 +0900

    fzf version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -225,7 +225,7 @@
 : ${gst_python_ver:=${gstreamer_ver}}
 : ${orc_ver:=0.4.32}
 : ${googletest_ver:=1.10.0}
-: ${fzf_ver:=0.28.0}
+: ${fzf_ver:=0.29.0}
 : ${bat_ver:=0.18.3}
 : ${jq_ver:=1.6}
 : ${libpcap_ver:=1.10.1}
