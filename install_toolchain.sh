ac3bcb9befe427c2cfe046eb3f7f22d99a1fc61c
payatsu <6873447+payatsu@users.noreply.github.com>
2021-12-29 03:43:03 +0900

    update install_native_opencv

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -6623,6 +6623,7 @@ install_native_opencv()
 	cmake `which ninja > /dev/null && echo -G Ninja` \
 		-S ${opencv_src_dir} -B ${opencv_bld_dir} \
 		-DCMAKE_C_COMPILER=${CC:-gcc} -DCMAKE_CXX_COMPILER=${CXX:-g++} \
+		-DCMAKE_CXX_STANDARD=17 \
 		-DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS} ${libdirs}" \
 		-DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS} ${libdirs}" \
 		-DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS} ${libdirs}" \
