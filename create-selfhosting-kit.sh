7e79da93f86caafbf9cf0541e3d77a93ed659c48
payatsu <6873447+payatsu@users.noreply.github.com>
2022-01-12 23:48:13 +0900

    add libXau to create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -227,6 +227,7 @@ EOF
 
 : ${util_macros_ver:=1.19.3}
 : ${xproto_ver:=7.0.31}
+: ${libXau_ver:=1.0.9}
 
 : ${prefix:=${default_prefix}}
 : ${host:=${default_host}}
@@ -526,6 +527,9 @@ fetch()
 	xproto)
 		eval wget -O \${${_1}_src_dir}.tar.gz \
 			https://xorg.freedesktop.org/archive/individual/proto/\${${_1:-xproto}_name}.tar.gz || return;;
+	libXau)
+		eval wget -O \${${_1}_src_dir}.tar.gz \
+			https://www.x.org/releases/individual/lib/\${${_1:-libX11}_name}.tar.gz || return;;
 	*) echo ERROR: not implemented. can not fetch \'${1}\'. >&2; return 1;;
 	esac
 }
@@ -3129,6 +3133,17 @@ EOF
 			${xproto_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules) || return
 		make -C ${xproto_bld_dir} -j ${jobs} || return
 		make -C ${xproto_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
+		;;
+	libXau)
+		[ -f ${DESTDIR}${prefix}/include/X11/Xauth.h -a "${force_install}" != yes ] && return
+		print_header_path Xproto.h X11 > /dev/null || ${0} ${cmdopt} xproto || return
+		fetch ${1} || return
+		unpack ${1} || return
+		[ -f ${libXau_bld_dir}/Makefile ] ||
+			(cd ${libXau_bld_dir}
+			${libXau_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules) || return
+		make -C ${libXau_bld_dir} -j ${jobs} || return
+		make -C ${libXau_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
 	*) echo ERROR: not implemented. can not build \'${1}\'. >&2; return 1;;
 	esac
