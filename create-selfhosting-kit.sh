3eebdb561bb884469d185c3c69155fee217b1e1c
payatsu <6873447+payatsu@users.noreply.github.com>
2022-01-13 23:13:39 +0900

    add fixesproto to create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -244,6 +244,7 @@ EOF
 : ${libXpm_ver:=3.5.11}
 : ${libXaw_ver:=1.0.14}
 : ${libXi_ver:=1.7}
+: ${fixesproto_ver:=5.0}
 
 : ${prefix:=${default_prefix}}
 : ${host:=${default_host}}
@@ -540,7 +541,7 @@ fetch()
 	util-macros)
 		wget -O ${util_macros_src_dir}.tar.gz \
 			https://xorg.freedesktop.org/archive/individual/util/${util_macros_name}.tar.gz || return;;
-	xproto|xcb-proto|xextproto|inputproto|kbproto)
+	xproto|xcb-proto|xextproto|inputproto|kbproto|fixesproto)
 		eval wget -O \${${_1}_src_dir}.tar.gz \
 			https://xorg.freedesktop.org/archive/individual/proto/\${${_1:-xproto}_name}.tar.gz || return;;
 	libXau|libXdmcp|xtrans|libICE|libSM|libxcb|libX11|libXext|libXt|libXmu|libXpm|libXaw|\
@@ -3409,6 +3410,18 @@ EOF
 				) || return
 		make -C ${libXi_bld_dir} -j ${jobs} || return
 		make -C ${libXi_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
+		;;
+	fixesproto)
+		[ -f ${DESTDIR}${prefix}/include/X11/extensions/xfixesproto.h -a "${force_install}" != yes ] && return
+		${0} ${cmdopt} util-macros || return
+		fetch ${1} || return
+		unpack ${1} || return
+		[ -f ${fixesproto_bld_dir}/Makefile ] ||
+			(cd ${fixesproto_bld_dir}
+			autoreconf -fiv -I ${DESTDIR}${prefix}/share/aclocal ${fixesproto_src_dir} || return
+			${fixesproto_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules) || return
+		make -C ${fixesproto_bld_dir} -j ${jobs} || return
+		make -C ${fixesproto_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
 	*) echo ERROR: not implemented. can not build \'${1}\'. >&2; return 1;;
 	esac
