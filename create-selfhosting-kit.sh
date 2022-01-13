971738991149ef0eb2ee34e435c8d1fc4414b1f7
payatsu <6873447+payatsu@users.noreply.github.com>
2022-01-13 21:28:43 +0900

    add libXmu to create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -240,6 +240,7 @@ EOF
 : ${libX11_ver:=1.7.2}
 : ${libXext_ver:=1.3.4}
 : ${libXt_ver:=1.2.1}
+: ${libXmu_ver:=1.1.3}
 
 : ${prefix:=${default_prefix}}
 : ${host:=${default_host}}
@@ -539,7 +540,7 @@ fetch()
 	xproto|xcb-proto|xextproto|inputproto|kbproto)
 		eval wget -O \${${_1}_src_dir}.tar.gz \
 			https://xorg.freedesktop.org/archive/individual/proto/\${${_1:-xproto}_name}.tar.gz || return;;
-	libXau|libXdmcp|xtrans|libICE|libSM|libxcb|libX11|libXext|libXt)
+	libXau|libXdmcp|xtrans|libICE|libSM|libxcb|libX11|libXext|libXt|libXmu)
 		eval wget -O \${${_1}_src_dir}.tar.gz \
 			https://www.x.org/releases/individual/lib/\${${_1:-libX11}_name}.tar.gz || return;;
 	*) echo ERROR: not implemented. can not fetch \'${1}\'. >&2; return 1;;
@@ -3325,6 +3326,24 @@ EOF
 				) || return
 		make -C ${libXt_bld_dir} -j ${jobs} || return
 		make -C ${libXt_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
+		;;
+	libXmu)
+		[ -f ${DESTDIR}${prefix}/include/X11/Xmu/Xmu.h -a "${force_install}" != yes ] && return
+		print_header_path lbx.h X11/extensions > /dev/null || ${0} ${cmdopt} xextproto || return
+		print_header_path Xlib.h X11 > /dev/null || ${0} ${cmdopt} libX11 || return
+		print_header_path Xext.h X11/extensions > /dev/null || ${0} ${cmdopt} libXext || return
+		print_header_path Core.h X11 > /dev/null || ${0} ${cmdopt} libXt || return
+		fetch ${1} || return
+		unpack ${1} || return
+		[ -f ${libXmu_bld_dir}/Makefile ] ||
+			(cd ${libXmu_bld_dir}
+			${libXmu_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules \
+				PKG_CONFIG_PATH= \
+				PKG_CONFIG_LIBDIR=`print_pkg_config_libdir` \
+				PKG_CONFIG_SYSROOT_DIR=${DESTDIR} \
+				) || return
+		make -C ${libXmu_bld_dir} -j ${jobs} || return
+		make -C ${libXmu_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
 	*) echo ERROR: not implemented. can not build \'${1}\'. >&2; return 1;;
 	esac
