0cbb42c4cd9d7e8d0cf424e01c773a06f7a024ce
payatsu <6873447+payatsu@users.noreply.github.com>
2022-01-23 19:04:05 +0900

    add gst-plugins-base to create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -302,6 +302,7 @@ EOF
 : ${gtk_ver:=3.24.31}
 
 : ${gstreamer_ver:=1.18.5}
+: ${gst_plugins_base_ver:=${gstreamer_ver}}
 
 : ${prefix:=${default_prefix}}
 : ${host:=${default_host}}
@@ -707,7 +708,7 @@ fetch()
 	libxkbcommon)
 		wget -O ${libxkbcommon_src_dir}.tar.xz \
 			https://xkbcommon.org/download/${libxkbcommon_name}.tar.xz || return;;
-	gstreamer)
+	gstreamer|gst-plugins-base)
 		eval wget -O \${${_1}_src_dir}.tar.xz \
 			https://gstreamer.freedesktop.org/src/${1:-gstreamer}/\${${_1:-gstreamer}_name}.tar.xz || return;;
 	*) echo ERROR: not implemented. can not fetch \'${1}\'. >&2; return 1;;
@@ -899,6 +900,7 @@ print_packages()
 		s/gdk_pixbuf/gdk-pixbuf/
 		s/at_spi2_core/at-spi2-core/
 		s/at_spi2_atk/at-spi2-atk/
+		s/gst_plugins_base/gst-plugins-base/
 	' || return
 }
 
@@ -4316,6 +4318,21 @@ EOF
 			${gstreamer_src_dir} ${gstreamer_bld_dir} || return
 		ninja -v -C ${gstreamer_bld_dir} || return
 		DESTDIR=${DESTDIR} ninja -v -C ${gstreamer_bld_dir} install || return
+		;;
+	gst-plugins-base)
+		[ -f ${DESTDIR}${prefix}/include/gstreamer-1.0/gst/video/video.h -a "${force_install}" != yes ] && return
+		which meson > /dev/null || ${0} ${cmdopt} --host ${build} --target ${build} meson || return
+		print_header_path gstversion.h gstreamer-1.0/gst > /dev/null || ${0} ${cmdopt} gstreamer || return
+		which orcc > /dev/null || ${0} ${cmdopt} --host ${build} --target ${build} orc || return
+		print_header_path jpeglib.h > /dev/null || ${0} ${cmdopt} jpeg || return
+		print_header_path png.h > /dev/null || ${0} ${cmdopt} libpng || return
+		fetch ${1} || return
+		unpack ${1} || return
+		meson --prefix ${prefix} ${strip:+--${strip}} --default-library both --cross-file ${cross_file} \
+			-Dc_link_args="${LDFLAGS} `l mount blkid gmodule-2.0 glib-2.0 unwind dw elf zstd lzma bz2 z ffi pcre`" \
+			${gst_plugins_base_src_dir} ${gst_plugins_base_bld_dir} || return
+		ninja -v -C ${gst_plugins_base_bld_dir} || return
+		DESTDIR=${DESTDIR} ninja -v -C ${gst_plugins_base_bld_dir} install || return
 		;;
 	*) echo ERROR: not implemented. can not build \'${1}\'. >&2; return 1;;
 	esac
