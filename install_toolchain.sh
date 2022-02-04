05493b00c59efdeb22d7399b54b37c05c3560006
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-04 21:44:10 +0900

    glibc version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -69,7 +69,7 @@
 : ${u_boot_ver:=2021.01}
 : ${qemu_ver:=6.1.0}
 : ${gperf_ver:=3.1}
-: ${glibc_ver:=2.34}
+: ${glibc_ver:=2.35}
 : ${newlib_ver:=4.1.0}
 : ${mingw_w64_ver:=9.0.0}
 : ${gmp_ver:=6.2.1}
