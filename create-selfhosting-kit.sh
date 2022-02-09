d70de4d46c06af2b904c38194205e942183504ee
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-09 20:25:55 +0900

    remove rpath from libXfixes in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -3972,10 +3972,13 @@ EOF
 		print_header_path Xproto.h X11 > /dev/null || ${0} ${cmdopt} xproto || return
 		print_header_path lbx.h X11/extensions > /dev/null || ${0} ${cmdopt} xextproto || return
 		print_header_path Xlib.h X11 > /dev/null || ${0} ${cmdopt} libX11 || return
+		${0} ${cmdopt} util-macros || return
 		fetch ${1} || return
 		unpack ${1} || return
 		[ -f ${libXfixes_bld_dir}/Makefile ] ||
 			(cd ${libXfixes_bld_dir}
+			autoreconf -fiv -I ${DESTDIR}${prefix}/share/aclocal ${libXfixes_src_dir} || return
+			remove_rpath_option ${1} || return
 			${libXfixes_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules \
 				PKG_CONFIG_SYSROOT_DIR=${DESTDIR} \
 				) || return
