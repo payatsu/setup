9d8a07beb53c2a95f06dcda33b80be52ad5500d4
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-13 23:25:33 +0900

    remove rpath from libSM in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -3790,7 +3790,9 @@ EOF
 			remove_rpath_option ${1} || return
 			${libSM_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules \
 				PKG_CONFIG_SYSROOT_DIR=${DESTDIR} \
-				) || return
+				|| return
+			remove_rpath_option ${1} || return
+			) || return
 		make -C ${libSM_bld_dir} -j ${jobs} || return
 		make -C ${libSM_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
