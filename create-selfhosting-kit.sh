9100e97d9fbace4100d1eec820820fc33f229a42
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-13 23:27:05 +0900

    remove rpath from libXaw in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -3981,7 +3981,9 @@ EOF
 			remove_rpath_option ${1} || return
 			${libXaw_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules \
 				PKG_CONFIG_SYSROOT_DIR=${DESTDIR} \
-				) || return
+				|| return
+			remove_rpath_option ${1} || return
+			) || return
 		make -C ${libXaw_bld_dir} -j ${jobs} || return
 		make -C ${libXaw_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
