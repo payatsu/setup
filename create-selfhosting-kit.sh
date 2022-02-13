4b222ebe42781b48305a61720dd686afb6357496
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-13 22:28:20 +0900

    remove rpath from Xrandr in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -4121,7 +4121,9 @@ EOF
 			${libXrandr_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules \
 				--enable-malloc0returnsnull \
 				PKG_CONFIG_SYSROOT_DIR=${DESTDIR} \
-				) || return
+				|| return
+			remove_rpath_option ${1} || return
+			) || return
 		make -C ${libXrandr_bld_dir} -j ${jobs} || return
 		make -C ${libXrandr_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
