9d0821c0d2805f53bc72915d90886e85d1db363b
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-13 23:21:28 +0900

    remove rpath from libX11 in create-selfhosting-kit.sh

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -3874,7 +3874,9 @@ EOF
 			${libX11_src_dir}/configure --prefix=${prefix} --build=${build} --host=${host} --disable-silent-rules \
 				--enable-malloc0returnsnull \
 				PKG_CONFIG_SYSROOT_DIR=${DESTDIR} \
-				) || return
+				|| return
+			remove_rpath_option ${1} || return
+			) || return
 		make -C ${libX11_bld_dir} -j ${jobs} || return
 		make -C ${libX11_bld_dir} -j ${jobs} DESTDIR=${DESTDIR} install${strip:+-${strip}} || return
 		;;
