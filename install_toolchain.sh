8bff6da415c8941c4654a2139c873695a57ca92b
payatsu <6873447+payatsu@users.noreply.github.com>
2022-02-19 17:24:19 +0900

    harfbuzz version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -290,7 +290,7 @@
 : ${pixman_ver:=0.40.0}
 : ${cairo_ver:=1.16.0}
 : ${fribidi_ver:=1.0.11}
-: ${harfbuzz_ver:=3.3.1}
+: ${harfbuzz_ver:=3.4.0}
 : ${pango_ver:=1.49.3}
 : ${pygobject_ver:=3.42.0}
 : ${itstool_ver:=2.0.7}
