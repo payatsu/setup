4f80e11e5448a0920b32ad3a9b3248d9142d35d7
payatsu <6873447+payatsu@users.noreply.github.com>
2022-04-01 18:52:38 +0900

    Imath version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -210,7 +210,7 @@
 : ${gflags_ver:=2.2.2}
 : ${glog_ver:=0.5.0}
 : ${openjpeg_ver:=2.4.0}
-: ${Imath_ver:=3.1.4}
+: ${Imath_ver:=3.1.5}
 : ${openexr_ver:=3.1.3}
 : ${eigen_ver:=3.4.0}
 : ${hdf5_ver:=1.12.1}
