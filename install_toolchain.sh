75cf8e92132f55300d5bcb690ac74a260b58800e
payatsu <6873447+payatsu@users.noreply.github.com>
2022-05-14 14:33:12 +0900

    bat version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -229,7 +229,7 @@
 : ${orc_ver:=0.4.32}
 : ${googletest_ver:=1.10.0}
 : ${fzf_ver:=0.30.0}
-: ${bat_ver:=0.20.0}
+: ${bat_ver:=0.21.0}
 : ${jq_ver:=1.6}
 : ${libpcap_ver:=1.10.1}
 : ${tcpdump_ver:=4.99.1}
