55a42b6d1049cc86b8ccf9103039ce5c590e3fb3
payatsu <6873447+payatsu@users.noreply.github.com>
2022-07-09 22:21:32 +0900

    meson version up

--- create-selfhosting-kit.sh
+++ create-selfhosting-kit.sh
@@ -229,7 +229,7 @@ EOF
 : ${go_ver:=1.18.1}
 : ${cmake_ver:=3.23.2}
 : ${ninja_ver:=1.11.0}
-: ${meson_ver:=0.62.2}
+: ${meson_ver:=0.63.0}
 : ${libxml2_ver:=2.9.11}
 : ${llvm_ver:=12.0.1}
 : ${compiler_rt_ver:=${llvm_ver}}
