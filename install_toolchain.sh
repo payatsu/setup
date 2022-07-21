ec689e9b3dddf92832438b181c671724f6169a14
payatsu <6873447+payatsu@users.noreply.github.com>
2022-07-22 00:09:51 +0900

    fzf version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -233,7 +233,7 @@
 : ${fftw_ver:=3.3.10}
 : ${ImageMagick_ver:=7.1.0-43}
 : ${googletest_ver:=1.10.0}
-: ${fzf_ver:=0.30.0}
+: ${fzf_ver:=0.31.0}
 : ${bat_ver:=0.21.0}
 : ${jq_ver:=1.6}
 : ${libpcap_ver:=1.10.1}
