3e72cff5fa827c0bb0ddd983a44b666ca427ca12
payatsu <6873447+payatsu@users.noreply.github.com>
2022-08-12 09:30:01 +0900

    rustc version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -187,7 +187,7 @@
 : ${cython_ver:=0.29.32}
 : ${OpenBLAS_ver:=0.3.21}
 : ${numpy_ver:=1.23.1}
-: ${rustc_ver:=1.62.1}
+: ${rustc_ver:=1.63.0}
 : ${rustup_ver:=1.25.1}
 : ${ruby_ver:=3.1.1}
 : ${go_ver:=1.19}
