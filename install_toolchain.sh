06702b29e16344e9fcd0c9c47e80f28f9d040591
payatsu <6873447+payatsu@users.noreply.github.com>
2022-12-27 18:13:14 +0900

    numpy version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -194,7 +194,7 @@
 : ${Python2_ver:=2.7.18}
 : ${cython_ver:=0.29.32}
 : ${OpenBLAS_ver:=0.3.21}
-: ${numpy_ver:=1.24.0}
+: ${numpy_ver:=1.24.1}
 : ${rustc_ver:=1.66.0}
 : ${rustup_ver:=1.25.1}
 : ${ruby_ver:=3.1.2}
