dda008765fe0ac4c581557914c108d9bdbf843d2
payatsu <6873447+payatsu@users.noreply.github.com>
2023-03-16 19:16:54 +0900

    ImageMagick version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -241,7 +241,7 @@
 : ${liblqr_ver:=0.4.2}
 : ${fftw_ver:=3.3.10}
 : ${LibRaw_ver:=0.21.1}
-: ${ImageMagick_ver:=7.1.1-2}
+: ${ImageMagick_ver:=7.1.1-3}
 : ${googletest_ver:=1.12.1}
 : ${fzf_ver:=0.38.0}
 : ${bat_ver:=0.22.1}
