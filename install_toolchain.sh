c1a6a1af6ec09923bb4cb405f32825b3cd99d9ba
payatsu <6873447+payatsu@users.noreply.github.com>
2023-03-23 22:43:39 +0900

    bazel version up

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -169,7 +169,7 @@
 : ${ninja_ver:=1.11.1}
 : ${meson_ver:=1.0.1}
 : ${cmake_ver:=3.26.0}
-: ${bazel_ver:=5.4.0}
+: ${bazel_ver:=6.1.1}
 : ${json_ver:=3.11.2}
 : ${fmt_ver:=9.1.0}
 : ${spdlog_ver:=1.11.0}
